1af613e73e54e9e69131d9b7adc913fe1468325d
tovesolve <toveso97@hotmail.com>
2022-03-02 10:46:05 +0100

    comments added

--- load_data/handle_df.py
+++ load_data/handle_df.py
@@ -1,3 +1,16 @@
+"""
+Shows characteristics of Dataset.
+
+
+Functions
+---------
+    * count_classes
+
+Run script:  python3 handle_df <args>
+    args are defined below
+Basic run:   python3 handle_df --df <file_name>
+"""
+
 import argparse
 from itertools import count
 from numpy import true_divide
@@ -5,8 +18,8 @@ import pandas as pd
 from sqlite3 import Timestamp
 import time
 
+# Arguments that can be run with script
 parser = argparse.ArgumentParser()
-
 parser.add_argument('--osm_name', dest='osm_name')
 parser.add_argument('--yelp_name', dest='yelp_name')
 parser.add_argument('--new_value', dest='new_value')
@@ -14,20 +27,42 @@ parser.add_argument('--df', dest = 'df')
 parser.add_argument('--class', dest = 'c')
 args = parser.parse_args()
 
-# Run script with name of both pois and new label (True/False)
-
 pd.set_option("display.max_rows", None, "display.max_columns", None) #show all rows when printing dataframe
 
-'''
-Counts and returns the number of each class together with a list of the true pairs (class1)
-'''
-def count_class(df):
+def count_classes(df):
+    '''
+    Counts and returns the number of each class together with lists of pairs of class0, class 1, class2 and class3
+    
+    Parameters
+    ----------
+    df : pandas DataFrame
+        Dataframe
+
+    Returns
+    -------
+    count_0
+        Number of pairs with class 0
+    count_1
+        Number of pairs with class 1
+    count_2
+        Number of pairs with class 2
+    count_3
+        Number of pairs with class 3
+    class0_pairs
+        list of pairs with class 0
+    class1_pairs
+        list of pairs with class 1
+    class2_pairs
+        list of pairs with class 2
+    class3_pairs
+        list of pairs with class 3
+    '''
     count_0 = 0
     count_1 = 0
     count_2 = 0
     count_3 = 0
-    false_pairs = []
-    true_pairs = []
+    class0_pairs = []
+    class1_pairs = []
     class2_pairs = []
     class3_pairs = []
     max_dist = 0
@@ -36,10 +71,10 @@ def count_class(df):
         ind = int(pair['match'])
         if  ind == 0:
             count_0 +=1
-            false_pairs.append((pair['osm_name'], pair['yelp_name'], pair['distance']))
+            class0_pairs.append((pair['osm_name'], pair['yelp_name'], pair['distance']))
         elif ind == 1:
             count_1 +=1
-            true_pairs.append((pair['osm_name'], pair['yelp_name'], pair['distance']))
+            class1_pairs.append((pair['osm_name'], pair['yelp_name'], pair['distance']))
             if float(pair['distance']) > max_dist:
                 max_dist = float(pair['distance'])
             sum_dist += float(pair['distance'])
@@ -49,18 +84,23 @@ def count_class(df):
         elif ind == 3:
             count_3 += 1
             class3_pairs.append((pair['osm_name'], pair['yelp_name'], pair['distance']))
-    
-    #for pair in true_pairs:
-    #    print(pair)
-    
-    print('max_dist', max_dist)
-    print('mean_dist ', sum_dist/count_1)
 
-    return count_0, count_1, count_2, count_3, false_pairs, true_pairs, class2_pairs, class3_pairs
+    print('max_dist', max_dist) # Print the maximum distance between to POIs with class 1
+    print('mean_dist ', sum_dist/count_1) # Print the mean distance between POIs in the pairs
+
+    return count_0, count_1, count_2, count_3, class0_pairs, class1_pairs, class2_pairs, class3_pairs
 
 
-# Wierd
 def extra_tags(true_pair_list):
+    '''
+    Prints tags from OSM POI from the true_pair_list
+        
+    Parameters
+    ----------
+    true_pair_list : list
+        List of pairs that have class 1
+
+    '''
     df_osm = pd.read_pickle('df_osm_florida2022-02-25.094449.pkl')
     df_yelp = pd.read_pickle('df_yelp_florida2022-02-25.093315.pkl')
     for tuple in true_pair_list:
@@ -71,17 +111,30 @@ def extra_tags(true_pair_list):
 
 
 def similar(true_pairs):
-    exact_pairs = []
-    exact_pairs_count = 0
-    similar_pairs = []
-    similar_pairs_count = 0
+    '''
+    Creates and returns seperate lists for pairs where POI names are an exact match and where they differ.
+    
+    Parameters
+    ----------
+    true_pairs : list
+        List of pairs that have class 1
+
+    Returns
+    -------
+    exact_pairs
+        list of pairs where the POIs have the exact same name
+    similar_pairs
+        list of pairs where the POIs have the similar names (are same point but name differs)
+    '''
+    exact_pairs = [] #Pairs where the POIs have the exact same name
+    similar_pairs = [] #Pairs where the POIs are the same point but have different names
+
     for pair in true_pairs:
         if pair[0] == pair[1]:
             exact_pairs.append(pair)
-            exact_pairs_count += 1
         else:
             similar_pairs.append(pair)
-            similar_pairs_count +=1
+
     print("===Exact matches: =====")
     for pair in exact_pairs:
         print(pair)
@@ -89,25 +142,24 @@ def similar(true_pairs):
     for pair in similar_pairs:
         print(pair)
     print("=============================================")
-    print("Number exact matches: ", exact_pairs_count, "     Number similar matches: ", similar_pairs_count)
-    return exact_pairs, similar_pairs, exact_pairs_count, similar_pairs_count
+    print("Number exact matches: ", len(exact_pairs), "     Number similar matches: ", len(similar_pairs))
+    return exact_pairs, similar_pairs
 
 
 
 def main():
-    df = pd.read_pickle(args.df)
+    df = pd.read_pickle(args.df) #Read dataframe from pickle
+
     print('number of pairs: ', df.shape[0])
-    count_0, count_1, count_2, count_3, false_pairs, true_pairs, class2_pairs, class3_pairs = count_class(df)
+    count_0, count_1, count_2, count_3, class0_pairs, class1_pairs, class2_pairs, class3_pairs = count_classes(df)
     print('Number class 0: ', count_0)
     print('Number class 1: ', count_1)
     print('Number class 2: ', count_2)
+    print('Number class 3: ', count_3)
     # for pair in class2_pairs:
     #     print(pair)
-    print('Number class 3: ', count_3)
-    for pair in class2_pairs:
-        print(pair)
-    #extra_tags(true_pairs)
-    similar(true_pairs)
+    
+    exact_pairs, similar_pairs = similar(class1_pairs)
     
 
 
--- load_data/label_data.py
+++ load_data/label_data.py
@@ -1,49 +1,81 @@
+"""
+Script for labeling data.
+
+Script allows user to manually label Points of Interest(POIs) from two different datasets. The script show the user two closely located POIs and the user can with an input value
+decide if the two POIs are a match or not.
+
+Functions
+---------
+    * close_pairs - Give user closely located POIs to label and saves labelled data to a DataFrame
+    * distance_meters - Calculate distance between two POIs
+    * restrict_dataset - Filter DataFrame according to coordinates
+    * main_florida - load POI datasets and start labeling
+    * main_boston - load POI datasets and start labeling
+
+Run script:                     python3 label_data.py
+Run script without warnings:    python3 -W ignore label_data.py
+
+"""
+
 import datetime
 import pandas as pd
 from sqlite3 import Timestamp
 from math import radians, sin, cos, sqrt, atan2
 import mpu
 
-# Run script without warnings: python3 -W ignore label_data.py
-
 pd.set_option("display.max_rows", None, "display.max_columns", None) #show all rows when printing dataframe
 
 
-'''
-Label closely located pairs and save to dataframe.
-'''
 def close_pairs(df_osm, df_yelp, distance):
-    data_colnames = ['osm_name', 'yelp_name', 'osm_latitude', 'osm_longitude', 'yelp_latitude', 'yelp_longitude', 'distance', 'match']
+    ''' Label POIs.
+    Selects closely located pairs from two datasets, label them: 0-no match, 1-match, 2-unsure, 3-not POIs.
+    Saves pairs to dataframe.
+
+    Parameters
+    ----------
+    df_osm : pandas DataFrame
+        Dataset from OSM data.
+    df_yelp : pandas DataFrame
+        Dataset from Yelp data.
+    distance : float
+        Defines how far away a POI can be from another in any of the four directions (north, south, west, east) to be considered as a possible pair.
+
+    Returns
+    -------
+    df_pairs
+        a DataFrame of the labelled pairs
+    '''
+
+    data_colnames = ['osm_name', 'yelp_name', 'osm_latitude', 'osm_longitude', 'yelp_latitude', 'yelp_longitude', 'distance', 'match'] #columns for the labelled dataset
     df_pairs = pd.DataFrame(columns=data_colnames)
-    checked_names = []
-    for index, poi_osm in df_osm.iterrows():
+
+    for index, poi_osm in df_osm.iterrows(): #iterate through POIs in OSM dataset
         lat_osm = float(poi_osm['latitude'])
         lon_osm = float(poi_osm['longitude'])
 
-        for i, poi_yelp in df_yelp.iterrows():
+        for i, poi_yelp in df_yelp.iterrows(): #iterate through POIs in yelp dataset
             lat_yelp = float(poi_yelp['latitude'])
             lon_yelp = float(poi_yelp['longitude'])
 
-            #restrict comparisons
+            #restrict only compare POIs if they are closely located
             if (lat_yelp < lat_osm+distance and lat_yelp > lat_osm-distance) and (lon_yelp < lon_osm+distance and lon_yelp > lon_osm-distance):     
-                alreadyLabeled = False
-                #om det finns en rad i df_pairs som redan innnehåller exakta paret.
-                for i, labeled_poi in df_pairs.iterrows():
-                    #print('checking:', labeled_poi['osm_name'], poi_osm['name'], labeled_poi['yelp_name'], poi_yelp['name'])
-                    if labeled_poi['osm_name'] == poi_osm['name'] and labeled_poi['yelp_name'] == poi_yelp['name']:
-                        alreadyLabeled = True
-                        print('already labeled', labeled_poi['osm_name'], poi_osm['name'], labeled_poi['yelp_name'], poi_yelp['name'])
+                alreadyLabelled = False
+                #check if pair already exist in labelled data:
+                for i, labelled_poi in df_pairs.iterrows():
+                    if labelled_poi['osm_name'] == poi_osm['name'] and labelled_poi['yelp_name'] == poi_yelp['name']:
+                        alreadyLabelled = True
+                        print('already labelled: ', labelled_poi['osm_name'], poi_osm['name'], labelled_poi['yelp_name'], poi_yelp['name'])
                         break
                         
-                if not alreadyLabeled:        
-                    poi_dist = distance_meters(lat_osm, lon_osm, lat_yelp, lon_yelp) #calculate distance betwenn POIs
-                    print(poi_osm['name'], " , ", poi_yelp['name'], ' distance between: ', poi_dist)
-
-                    if poi_osm['name'].lower() == poi_yelp['name'].lower():
+                if not alreadyLabelled:        
+                    poi_dist = distance_meters(lat_osm, lon_osm, lat_yelp, lon_yelp) #calculate distance between POIs
+                    
+                    print(poi_osm['name'], " , ", poi_yelp['name'], ' distance between: ', poi_dist) #Print pair for user to consider
+                    if poi_osm['name'].lower() == poi_yelp['name'].lower(): #if names are an exact match
                         df_pairs = df_pairs.append({'osm_name': poi_osm['name'], 'yelp_name': poi_yelp['name'], 'osm_latitude': poi_osm['latitude'], 'osm_longitude': poi_osm['longitude'], 'yelp_latitude': poi_yelp['latitude'], 'yelp_longitude': poi_yelp['longitude'], 'distance': poi_dist, 'match': 1}, ignore_index=True)
                     else:
                         while True:
-                            str_num = input()
+                            str_num = input() #Take input from user
                             try:
                                 num = int(str_num)
                                 if num not in range(0,4): #restrict input to 0/1
@@ -53,32 +85,67 @@ def close_pairs(df_osm, df_yelp, distance):
                             except ValueError:
                                 print("not int, try again")
 
+                        #Add pair to DataFrame:
                         if num == 1:
                             df_pairs = df_pairs.append({'osm_name': poi_osm['name'], 'yelp_name': poi_yelp['name'], 'osm_latitude': poi_osm['latitude'], 'osm_longitude': poi_osm['longitude'], 'yelp_latitude': poi_yelp['latitude'], 'yelp_longitude': poi_yelp['longitude'], 'distance': poi_dist, 'match': 1}, ignore_index=True)
-                            #print('pair added')
                         elif num == 0:
                             df_pairs = df_pairs.append({'osm_name': poi_osm['name'], 'yelp_name': poi_yelp['name'], 'osm_latitude': poi_osm['latitude'], 'osm_longitude': poi_osm['longitude'], 'yelp_latitude': poi_yelp['latitude'], 'yelp_longitude': poi_yelp['longitude'], 'distance': poi_dist, 'match': 0}, ignore_index=True)
-                            #print('pair added')
                         elif num == 2:
                             df_pairs = df_pairs.append({'osm_name': poi_osm['name'], 'yelp_name': poi_yelp['name'], 'osm_latitude': poi_osm['latitude'], 'osm_longitude': poi_osm['longitude'], 'yelp_latitude': poi_yelp['latitude'], 'yelp_longitude': poi_yelp['longitude'], 'distance': poi_dist, 'match': 2}, ignore_index=True)
-                            #print('pair added')
                         elif num == 3:
                             df_pairs = df_pairs.append({'osm_name': poi_osm['name'], 'yelp_name': poi_yelp['name'], 'osm_latitude': poi_osm['latitude'], 'osm_longitude': poi_osm['longitude'], 'yelp_latitude': poi_yelp['latitude'], 'yelp_longitude': poi_yelp['longitude'], 'distance': poi_dist, 'match': 3}, ignore_index=True)
-                            #print('pair added')
 
-    df_pairs.to_pickle('./df_pairs_boston' + str(datetime.datetime.now().strftime("%Y-%m-%d.%H%M%S")) + '.pkl') # save dataframe
+    df_pairs.to_pickle('./df_pairs_boston' + str(datetime.datetime.now().strftime("%Y-%m-%d.%H%M%S")) + '.pkl') # save dataframe to pickle
     return df_pairs
 
-'''
-Calculate distance between two places on earth.
-'''
+
 def distance_meters(lat1, lon1, lat2, lon2):
+    '''
+    Calculate distance between two places (POI1, POI2) on earth.
+
+    Parameters
+    ----------
+    lat1 : float
+        Latitude of POI1
+    lon1 : float
+        Longitude of POI1
+    lat2 : float
+        Latitude of POI2
+    lon2 : float
+        Longitude of POI2
+    
+    Returns
+    -------
+    dist
+        the distance between the points in meters
+    '''
     dist = mpu.haversine_distance((lat1, lon1), (lat2, lon2))
-    dist = dist * 1000 # convert to meters
+    dist = dist * 1000 # convert distance to meters
     #print(dist)
     return dist
 
 def restrict_dataset(df, top, bottom, left, right):
+    '''
+    Filter the DataFrame of POIs according to coordinates.
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+        The DataFrame to filter
+    top : float
+        Latitude coordinate limit area from north.
+    bottom : float
+        Latitude coordinate limit area from south.
+    left : float
+        Latitude coordinate limit area from west.
+    right : float
+        Latitude coordinate limit area from right.
+    
+    Returns
+    -------
+    df
+        The filtered DataFrame
+    '''
     df = df[pd.to_numeric(df['latitude']) < top]
     df = df[pd.to_numeric(df['latitude']) > bottom]
     df = df[pd.to_numeric(df['longitude']) > left]
@@ -87,35 +154,39 @@ def restrict_dataset(df, top, bottom, left, right):
     
 
 def main_florida():
+    '''
+    Main method for Florida area
+    '''
     df_osm = pd.read_pickle('df_osm_florida2022-02-25.094449.pkl')   #read dataframe osm data
     df_yelp = pd.read_pickle('df_yelp_florida2022-02-25.093315.pkl') #read dataframe yelp data
 
-    df_osm = restrict_dataset(df_osm, 28.616707, 28.567949, -81.405150, -81.315333) # 28.591826, 28.567067, -81.372633, -81.303579
-    df_yelp = restrict_dataset(df_yelp, 28.616707, 28.567949, -81.405150, -81.315333) # 28.591826, 28.567067, -81.372633, -81.303579
-    print('Number of rows in labeled df_osm: ', df_osm.shape[0])
-    print('Number of rows in labeled df_yelp: ', df_yelp.shape[0])
+    df_osm = restrict_dataset(df_osm, 28.616707, 28.567949, -81.405150, -81.315333) 
+    df_yelp = restrict_dataset(df_yelp, 28.616707, 28.567949, -81.405150, -81.315333)
+    print('Number of rows in labelled df_osm: ', df_osm.shape[0])
+    print('Number of rows in labelled df_yelp: ', df_yelp.shape[0])
 
     df = close_pairs(df_osm, df_yelp, distance=0.0002)  #0.001=111m
     print("Dataframe pairs:")
     print(df)
-    print('Number of rows in labeled df: ', df.shape[0])
+    print('Number of rows in labelled df: ', df.shape[0])
 
 
 def main_boston():
+    '''
+    Main method for Boston area
+    '''
     df_osm = pd.read_pickle('df_osm_ma2022-02-25.104802.pkl')   #read dataframe osm data
     df_yelp = pd.read_pickle('df_yelp_ma2022-02-25.103519.pkl') #read dataframe yelp data
 
-    #boston coordinates: (42.366625, 42.349203, -71.068092, -71.055022)
-
-    df_osm = restrict_dataset(df_osm, 42.366625, 42.349203, -71.068092, -71.055022) # 28.591826, 28.567067, -81.372633, -81.303579
-    df_yelp = restrict_dataset(df_yelp, 42.366625, 42.349203, -71.068092, -71.055022) # 28.591826, 28.567067, -81.372633, -81.303579
+    df_osm = restrict_dataset(df_osm, 42.366625, 42.349203, -71.068092, -71.055022) 
+    df_yelp = restrict_dataset(df_yelp, 42.366625, 42.349203, -71.068092, -71.055022)
     print('Number of rows in osm df: ', df_osm.shape[0])
     print('Number of rows in yelp df: ', df_yelp.shape[0])
 
     df = close_pairs(df_osm, df_yelp, distance=0.0002)  #0.001=111m
     print("Dataframe pairs:")
     print(df)
-    print('Number of rows in labeled df: ', df.shape[0])
+    print('Number of rows in labelled df: ', df.shape[0])
 
 
 def main():
