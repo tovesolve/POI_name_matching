c0be377f2036c779129ab555a865c5a1525af90d
tovesolve <toveso97@hotmail.com>
2022-03-03 15:30:22 +0100

    Baseline script

---
+++ load_data/baseline.py
@@ -0,0 +1,46 @@
+from character_based_func import *
+from drop_label import drop_rows_with_label
+from evaluation_metrics import *
+from nltk.metrics.distance import edit_distance
+from nltk.metrics.distance import jaro_similarity as jaro
+from nltk.metrics.distance import jaro_winkler_similarity as jaro_wrinkler
+from sklearn.metrics import precision_score, recall_score, f1_score
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+# Baseline
+# for list of sim_func:
+    #for thresholds in threshold_list:
+        #calculate sim_score (vi har nu y_predicted och y_truevalues)
+        #calculate get_metrics
+        #add f1 to a object together with sim_func and threshold
+# plotta graf utifrån lista med objekt
+def baseline_script(df, sim_func_list, threshold_list):
+    dict = {}
+    for sim_func in sim_func_list:
+        f1_scores = []
+        for threshold in threshold_list:
+            df_scores = calculate_similarity_score(df, sim_func)
+            df_scores = score_to_label(df_scores, threshold)
+            precision, recall, f1_score, matthew = get_metrics(df_scores)
+            f1_scores.append(f1_score)
+        dict[sim_func] = f1_scores
+    #plot graph
+    f1_comparision_graph(dict, threshold_list, sim_func_list)
+
+
+
+def main():
+    pd.set_option("display.max_rows", None, "display.max_columns", None) #show all rows when printing dataframe
+
+    #df = pd.read_pickle('v0_df_pairs_florida2022-02-28.094015.pkl')
+    df = pd.read_pickle('v0_df_pairs_boston2022-02-28.110406.pkl')
+    
+    df = drop_rows_with_label(df, 2)
+    baseline_script(df, sim_func_list=[levenshtein_similarity, damarau_levenshtein_similarity, jaro_similarity], threshold_list=[0.5, 0.7, 1])
+
+
+if __name__ == "__main__":
+    main()
--- load_data/evaluation_metrics.py
+++ load_data/evaluation_metrics.py
@@ -1,3 +1,4 @@
+from tkinter import CENTER
 from sklearn.metrics import precision_score, recall_score, f1_score, matthews_corrcoef, confusion_matrix, ConfusionMatrixDisplay, plot_confusion_matrix
 import numpy as np
 import pandas as pd
@@ -20,20 +21,42 @@ def get_metrics(df):
     f1 = f1_score(df['match'].tolist(), df['score'].tolist())
     matthew = matthews_corrcoef(df['match'].tolist(), df['score'].tolist())
     cm = get_confusion_matrix(df)
-    #f = sns.heatmap(cm, annot=True)
-    #print(f)
-    #examplePlot()
-    #plot_confusion_matrix(df['match'].tolist(), df['score'].tolist(), classes=[0,1], title="cm")
-    example()
-    display_labels=[0,1]
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=display_labels)
-    disp.plot()
-    plt.show()
+    #display_confusion_matrix(cm)
     return precision, recall, f1, matthew
 
 def get_confusion_matrix(df):
     return confusion_matrix(df['match'].tolist(), df['score'].tolist())
+
+def display_confusion_matrix(cm):
+    display_labels=[0,1]
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=display_labels)
+    disp.plot()
+    plt.show()
+
+def f1_comparision_graph(metrics_object_list, threshold_list, sim_func_list):
+    X = threshold_list #thresholds
     
+    X_axis = np.arange(len(X))
+
+    nbr_of_funcs = len(sim_func_list)
+    width = 0.2
+    total_width = width * nbr_of_funcs
+    start = X_axis - total_width/2 + width/2
+    for sim_func in sim_func_list:
+        f1_value = metrics_object_list[sim_func]
+        plt.bar(start, f1_value, width, label = str(sim_func.__name__))
+        for i in range(len(f1_value)):
+            plt.text(start[i], f1_value[i] + 0.01, str(round(f1_value[i],2)))
+        start = start + width
+
+    plt.xticks(X_axis, X)
+    plt.xlabel("Similarity functions")
+    plt.ylabel("f1-score")
+    plt.title("f1-score of different similarity functions")
+    plt.grid(axis='y')
+    plt.legend()
+    plt.show()
+
 def examplePlot():
     # Data for plotting
     t = np.arange(0.0, 2.0, 0.01)
@@ -48,32 +71,20 @@ def examplePlot():
 
     fig.savefig("test.png")
     plt.show()
-
-def example():
-    # x axis values 
-    x = [1,2,3] 
-    # corresponding y axis values 
-    y = [2,4,1] 
-        
-    # plotting the points  
-    plt.plot(x, y) 
-        
-    # naming the x axis 
-    plt.xlabel('x - axis') 
-    # naming the y axis 
-    plt.ylabel('y - axis') 
-        
-    # giving a title to my graph 
-    plt.title('My first graph!') 
-        
-    # function to show the plot 
-    plt.show()     
+   
     
 def main():
     pd.set_option("display.max_rows", None, "display.max_columns", None) #show all rows when printing dataframe
 
     #df = pd.read_pickle('v0_df_pairs_florida2022-02-28.094015.pkl')
-    df = pd.read_pickle('df_pairs_boston2022-02-28.110406.pkl')
+    #df = pd.read_pickle('df_pairs_boston2022-02-28.110406.pkl')
+    f1_comparision_graph(metrics_object_list={'levenstein':[0.31,20,30], 'damarau': [10, 10, 10], 'jaro': [5,5,5]}, threshold_list= [1,2,3], sim_func_list=['levenstein', 'damarau', 'jaro'])
+    f1_comparision_graph(metrics_object_list={'levenstein':[10,20,30], 'damarau': [10, 10, 10]}, threshold_list= [1,2,3], sim_func_list=['levenstein', 'damarau'])
+
+    #examplePlot()
 
 if __name__ == "__main__":
     main()
+
+
+
