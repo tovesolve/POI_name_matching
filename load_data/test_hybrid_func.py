92218435b26c823b99a09acea3ef66e7685638fc
Lalbinsson <lalbinsson@outlook.com>
2022-03-31 08:58:44 +0200

    semantic similarity with softTFIDF

--- load_data/test_hybrid_func.py
+++ load_data/test_hybrid_func.py
@@ -5,6 +5,8 @@ from baseline import calculate_similarity_score
 from drop_label import *
 from token_based_func import *
 from evaluation_metrics import *
+from word_embeddings import *
+from word_embeddings_cosine import *
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -20,7 +22,7 @@ from character_based_func import jaro_winkler_similarity, levenshtein_similarity
 #from py_stringmatching.similarity_measure.hybrid_similarity_measure import \
 #                                                    HybridSimilarityMeasure
 
-def softTFIDF(df, secondary_func=levenshtein_similarity, secondary_threshold = 0.5):
+def softTFIDF(df, model, char_func=levenshtein_similarity, semantic_threshold = 0.6, char_threshold = 0.5):
     corpus_list = get_corpus_list_for_pystringmatching(df) #create corpus from dataframe
 
     #manual softTFIDF:
@@ -34,7 +36,7 @@ def softTFIDF(df, secondary_func=levenshtein_similarity, secondary_threshold = 0
     data_colnames = ['osm_name', 'yelp_name', 'osm_latitude', 'osm_longitude', 'yelp_latitude', 'yelp_longitude', 'distance', 'match', 'score']
     df_scores = pd.DataFrame(columns=data_colnames) #create dataframe where similarity score can be added to pairs
     for index, pair in df.iterrows():
-        score = calc_softTFIDF_for_pair(pair['osm_name'], pair['yelp_name'], corpus_list, secondary_threshold, secondary_func, document_frequency)
+        score = calc_softTFIDF_for_pair(pair['osm_name'], pair['yelp_name'], corpus_list, char_threshold, char_func, semantic_threshold, model, document_frequency)
         #score = calc_softTFIDF_for_pair("Park Avenue Pizza", "Park Ave Pizza", corpus_list, secondary_threshold, secondary_func, document_frequency)
         
         df_scores = df_scores.append({'osm_name': pair['osm_name'], 'yelp_name': pair['yelp_name'], 'osm_latitude': pair['osm_latitude'], 'osm_longitude': pair['osm_longitude'], 'yelp_latitude': pair['yelp_latitude'], 'yelp_longitude': pair['yelp_longitude'], 'distance': pair['distance'], 'match': pair['match'], 'score': score}, ignore_index=True)
@@ -49,7 +51,7 @@ def sim_check_for_exact_match(*args):
     if args[0] == args[1]:
         return True
 
-def calc_softTFIDF_for_pair(osm_name, yelp_name, corpus_list, threshold, secondary_func, document_frequency):
+def calc_softTFIDF_for_pair(osm_name, yelp_name, corpus_list, char_threshold, char_func, semantic_threshold, model, document_frequency):
     tokenized_osm_name = tokenize(osm_name)
     tokenized_yelp_name = tokenize(yelp_name)
     tf_x, tf_y = collections.Counter(tokenized_osm_name), collections.Counter(tokenized_yelp_name)
@@ -78,14 +80,25 @@ def calc_softTFIDF_for_pair(osm_name, yelp_name, corpus_list, threshold, seconda
         #print(term_x)
         max_score = 0.0
         for term_y in tf_y:
-            #score = levenshtein_similarity(term_x, term_y)
-            score = secondary_func(term_x, term_y)
-            #print("levenshtein for ", term_x, " and ", term_y, ": ", score)
-            # adding sim only if it is above threshold and
-            # highest for this element
-            if score >= threshold and score > max_score:
-                similarity_map[term_x] = (term_x, term_y, score)
-                max_score = score
+            #character-based similarity func
+            char_score = char_func(term_x, term_y)
+            #print("char score for ", term_x, " and ", term_y, ": ", char_score)
+            #semantic similarity with sBERT    
+            embedding_x = sBERT(term_x, model)
+            embedding_y = sBERT(term_y, model)
+            semantic_score = sklearn_cosine_similarity(embedding_x, embedding_y)[0][0]
+            #print("semantic score for ", term_x, " and ", term_y, ": ", semantic_score)
+            
+            if (char_score >= char_threshold or semantic_score >= semantic_threshold):
+                score = max(char_score, semantic_score)
+                # if score < 1.0:
+                #     print("char score for ", term_x, " and ", term_y, ": ", char_score)
+                #     print("semantic score for ", term_x, " and ", term_y, ": ", semantic_score)
+                #print("score for terms: ", score)
+                if score > max_score:
+                    #print("saving max score: ", score)
+                    similarity_map[term_x] = (term_x, term_y, score)
+                    max_score = score
     
     first_string_pos = 0
     second_string_pos = 1
@@ -129,118 +142,129 @@ def calc_softTFIDF_for_pair(osm_name, yelp_name, corpus_list, threshold, seconda
     #print(score)
     return score
 
-def calc_softTFIDF_for_pair_package(osm_name, yelp_name, corpus_list, threshold, secondary_func, document_frequency):
-    tokenized_osm_name = tokenize(osm_name)
-    tokenized_yelp_name = tokenize(yelp_name)
-    soft_tfidf = py_stringmatching.SoftTfIdf(corpus_list, sim_func=secondary_func, threshold=threshold)
-    package_score = soft_tfidf.get_raw_score(tokenized_osm_name, tokenized_yelp_name)
-    return package_score
+# def calc_softTFIDF_for_pair_package(osm_name, yelp_name, corpus_list, threshold, char_func, document_frequency):
+#     tokenized_osm_name = tokenize(osm_name)
+#     tokenized_yelp_name = tokenize(yelp_name)
+#     soft_tfidf = py_stringmatching.SoftTfIdf(corpus_list, sim_func=char_func, threshold=threshold)
+#     package_score = soft_tfidf.get_raw_score(tokenized_osm_name, tokenized_yelp_name)
+#     return package_score
 
 
-def TFIDF(df):
-    corpus_list = get_corpus_list_for_pystringmatching(df) #create corpus from dataframe
+# def TFIDF(df):
+#     corpus_list = get_corpus_list_for_pystringmatching(df) #create corpus from dataframe
 
-    #manual TFIDF:
-    document_frequency = {}
-    if corpus_list != None:
-        for document in corpus_list:
-            for element in set(document):
-                document_frequency[element] = (document_frequency.get(element, 0) + 1)
-    #print("doc freq:", document_frequency)
+#     #manual TFIDF:
+#     document_frequency = {}
+#     if corpus_list != None:
+#         for document in corpus_list:
+#             for element in set(document):
+#                 document_frequency[element] = (document_frequency.get(element, 0) + 1)
+#     #print("doc freq:", document_frequency)
 
-    data_colnames = ['osm_name', 'yelp_name', 'osm_latitude', 'osm_longitude', 'yelp_latitude', 'yelp_longitude', 'distance', 'match', 'score']
-    df_scores = pd.DataFrame(columns=data_colnames) #create dataframe where similarity score can be added to pairs
-    for index, pair in df.iterrows():
-        score = calc_tfidf_for_pair(pair['osm_name'], pair['yelp_name'], corpus_list, document_frequency)
-        df_scores = df_scores.append({'osm_name': pair['osm_name'], 'yelp_name': pair['yelp_name'], 'osm_latitude': pair['osm_latitude'], 'osm_longitude': pair['osm_longitude'], 'yelp_latitude': pair['yelp_latitude'], 'yelp_longitude': pair['yelp_longitude'], 'distance': pair['distance'], 'match': pair['match'], 'score': score}, ignore_index=True)
+#     data_colnames = ['osm_name', 'yelp_name', 'osm_latitude', 'osm_longitude', 'yelp_latitude', 'yelp_longitude', 'distance', 'match', 'score']
+#     df_scores = pd.DataFrame(columns=data_colnames) #create dataframe where similarity score can be added to pairs
+#     for index, pair in df.iterrows():
+#         score = calc_tfidf_for_pair(pair['osm_name'], pair['yelp_name'], corpus_list, document_frequency)
+#         df_scores = df_scores.append({'osm_name': pair['osm_name'], 'yelp_name': pair['yelp_name'], 'osm_latitude': pair['osm_latitude'], 'osm_longitude': pair['osm_longitude'], 'yelp_latitude': pair['yelp_latitude'], 'yelp_longitude': pair['yelp_longitude'], 'distance': pair['distance'], 'match': pair['match'], 'score': score}, ignore_index=True)
 
-    return df_scores
+#     return df_scores
 
 
-def calc_tfidf_for_pair(osm_name, yelp_name, corpus_list, document_frequency):
-    tokenized_osm_name = tokenize(osm_name)
-    tokenized_yelp_name = tokenize(yelp_name)
-    tf_x, tf_y = collections.Counter(tokenized_osm_name), collections.Counter(tokenized_yelp_name)
+# def calc_tfidf_for_pair(osm_name, yelp_name, corpus_list, document_frequency):
+#     tokenized_osm_name = tokenize(osm_name)
+#     tokenized_yelp_name = tokenize(yelp_name)
+#     tf_x, tf_y = collections.Counter(tokenized_osm_name), collections.Counter(tokenized_yelp_name)
 
-    # if the strings match exactly return 1.0
-    if sim_check_for_exact_match(tokenized_osm_name, tokenized_yelp_name):
-        return 1.0
+#     # if the strings match exactly return 1.0
+#     if sim_check_for_exact_match(tokenized_osm_name, tokenized_yelp_name):
+#         return 1.0
 
-    # if one of the strings is empty return 0
-    if sim_check_for_empty(tokenized_osm_name, tokenized_yelp_name):
-        return 0.0
+#     # if one of the strings is empty return 0
+#     if sim_check_for_empty(tokenized_osm_name, tokenized_yelp_name):
+#         return 0.0
 
-    # find unique elements in the input lists and their document frequency 
-    local_df = {}
-    for element in tf_x:
-        local_df[element] = local_df.get(element, 0) + 1
-    for element in tf_y:
-        local_df[element] = local_df.get(element, 0) + 1
+#     # find unique elements in the input lists and their document frequency 
+#     local_df = {}
+#     for element in tf_x:
+#         local_df[element] = local_df.get(element, 0) + 1
+#     for element in tf_y:
+#         local_df[element] = local_df.get(element, 0) + 1
 
-    # # if corpus is not provided treat input string as corpus
-    corpus_size = len(corpus_list)
-    curr_df, corpus_size = (local_df, 2) if corpus_list is None else ((document_frequency, corpus_size))
+#     # # if corpus is not provided treat input string as corpus
+#     corpus_size = len(corpus_list)
+#     curr_df, corpus_size = (local_df, 2) if corpus_list is None else ((document_frequency, corpus_size))
    
-    #manual tfidf:    
-    idf_element, v_x, v_y, v_x_y, v_x_2, v_y_2 = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-    # tfidf calculation
-    for element in local_df.keys():
-        df_element = curr_df.get(element)
-        if df_element is None:
-            continue
-        idf_element = corpus_size * 1.0 / df_element
-        v_x = 0 if element not in tf_x else (log(idf_element) * log(tf_x[element] + 1)) if True else (
-                idf_element * tf_x[element])
-        v_y = 0 if element not in tf_y else (log(idf_element) * log(tf_y[element] + 1)) if True else (
-                idf_element * tf_y[element])
-        v_x_y += v_x * v_y
-        #print(element)
-        #print("vx: ", v_x)
-        v_x_2 += v_x * v_x
-        #print(v_y)
-        v_y_2 += v_y * v_y
-    print("result manual tfidf:")
-    score = (0.0 if v_x_y == 0 else v_x_y / (sqrt(v_x_2) * sqrt(v_y_2)))   
-    print(score)
-    return score
+#     #manual tfidf:    
+#     idf_element, v_x, v_y, v_x_y, v_x_2, v_y_2 = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+#     # tfidf calculation
+#     for element in local_df.keys():
+#         df_element = curr_df.get(element)
+#         if df_element is None:
+#             continue
+#         idf_element = corpus_size * 1.0 / df_element
+#         v_x = 0 if element not in tf_x else (log(idf_element) * log(tf_x[element] + 1)) if True else (
+#                 idf_element * tf_x[element])
+#         v_y = 0 if element not in tf_y else (log(idf_element) * log(tf_y[element] + 1)) if True else (
+#                 idf_element * tf_y[element])
+#         v_x_y += v_x * v_y
+#         #print(element)
+#         #print("vx: ", v_x)
+#         v_x_2 += v_x * v_x
+#         #print(v_y)
+#         v_y_2 += v_y * v_y
+#     print("result manual tfidf:")
+#     score = (0.0 if v_x_y == 0 else v_x_y / (sqrt(v_x_2) * sqrt(v_y_2)))   
+#     print(score)
+#     return score
                     
 
-def calc_TFIDF_for_pair_package(osm_name, yelp_name, corpus_list, document_frequency):
-    tokenized_osm_name = tokenize(osm_name)
-    tokenized_yelp_name = tokenize(yelp_name)
-    tfidf = py_stringmatching.TfIdf(corpus_list, dampen=True)
-    package_score = tfidf.get_sim_score(tokenized_osm_name, tokenized_yelp_name)
-    print("result tfidf from package:")
-    print(package_score)
-    return package_score
+# def calc_TFIDF_for_pair_package(osm_name, yelp_name, corpus_list, document_frequency):
+#     tokenized_osm_name = tokenize(osm_name)
+#     tokenized_yelp_name = tokenize(yelp_name)
+#     tfidf = py_stringmatching.TfIdf(corpus_list, dampen=True)
+#     package_score = tfidf.get_sim_score(tokenized_osm_name, tokenized_yelp_name)
+#     print("result tfidf from package:")
+#     print(package_score)
+#     return package_score
 
 
-def tfidf_script(df, sim_funcs, primary_thresholds, secondary_thresholds, metric):
+def tfidf_script(df, sim_funcs, primary_thresholds, char_thresholds, semantic_threshold, metric):
     dict = {}
+    
+    model_sBERT = SentenceTransformer("all-mpnet-base-v2")
+    
     for sim_func in sim_funcs:
         scores = []           
         for primary_threshold in primary_thresholds:
-            for secondary_threshold in secondary_thresholds:
-                df_scores = softTFIDF(df, secondary_func=sim_func, secondary_threshold = secondary_threshold)
+            for char_threshold in char_thresholds:
+                df_scores = softTFIDF(df, model_sBERT, char_func=sim_func, semantic_threshold = semantic_threshold, char_threshold = char_threshold)
                 #df_scores = TFIDF(df)
 
                 print("=========================False positives:========================================")
+                count_fp = 0
                 for index, pair in df_scores.iterrows():
                     if (pair['match'] is 0) and pair['score'] >= primary_threshold:
                         print(pair['osm_name'], "    ", pair['yelp_name'], "    match: ", pair['match'], "  score: ", pair['score'])
                         print("tokenized to: ", tokenize(pair['osm_name']), " and: ", tokenize(pair['yelp_name']))
+                        count_fp += 1
+                print("count False positives: ", count_fp)
+                        
 
                 print("==========================Flase negatives:========================================")
+                count_fn = 0
                 for index, pair in df_scores.iterrows():
                     if (pair['match'] is 1) and pair['score'] <= primary_threshold:
                         print(pair['osm_name'], "    ", pair['yelp_name'], "    match: ", pair['match'], "  score: ", pair['score'])
                         print("tokenized to: ", tokenize(pair['osm_name']), " and: ", tokenize(pair['yelp_name']))
+                        count_fn += 1
+                print("count False negatives: ", count_fn)
 
-                print("==========================True positives:========================================")
-                for index, pair in df_scores.iterrows():
-                    if (pair['match'] == 1) and pair['score'] >= primary_threshold:
-                        print(pair['osm_name'], "    ", pair['yelp_name'], "    match: ", pair['match'], "  score: ", pair['score'])
-                        print("tokenized to: ", tokenize(pair['osm_name']), " and: ", tokenize(pair['yelp_name']))
+
+                # print("==========================True positives:========================================")
+                # for index, pair in df_scores.iterrows():
+                #     if (pair['match'] == 1) and pair['score'] >= primary_threshold:
+                #         print(pair['osm_name'], "    ", pair['yelp_name'], "    match: ", pair['match'], "  score: ", pair['score'])
+                #         print("tokenized to: ", tokenize(pair['osm_name']), " and: ", tokenize(pair['yelp_name']))
 
 
 
@@ -260,11 +284,11 @@ def tfidf_script(df, sim_funcs, primary_thresholds, secondary_thresholds, metric
     print(dict)
     threshold_tuples = [] 
     for i in range(len(primary_thresholds)):
-        for j in range(len(secondary_thresholds)):
-            threshold_tuples.append((primary_thresholds[i], secondary_thresholds[j]))
+        for j in range(len(char_thresholds)):
+            threshold_tuples.append((primary_thresholds[i], char_thresholds[j]))
     print(threshold_tuples)
 
-    plot_evaluation_graph(dict, threshold_tuples, sim_funcs, metric)
+    #plot_evaluation_graph(dict, threshold_tuples, sim_funcs, metric)
 
 
 def main():
@@ -278,8 +302,9 @@ def main():
     df = pd.concat([df1, df2, df3, df4, df5])
     df = drop_rows_with_label(df, 3)
     df = drop_rows_with_label(df, 2)
-    #df = drop_exact_rows(df)
-    tfidf_script(df, [jaro_winkler_similarity], [0.3],[0.85], 'f1_score')
+    df = drop_exact_rows(df)
+    semantic_threshold = 0.7
+    tfidf_script(df, [jaro_winkler_similarity], [0.3],[0.85], semantic_threshold, 'f1_score')
     # df_with_scores = softTFIDF(df, secondary_func=jaro_winkler_similarity, secondary_threshold=0.8)
     # #df_with_scores = TFIDF(df, secondary_func=jaro_winkler_similarity, secondary_threshold=0.8)
     
